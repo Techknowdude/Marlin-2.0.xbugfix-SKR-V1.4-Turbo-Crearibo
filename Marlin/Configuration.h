9a82cbf08421485c8b625002ed1437bef18ab6e6
Brandon Westmoreland <Techknowdude@users.noreply.github.com>
2021-08-25 12:29:32 -0700

    Updated Z offset for binlinear ABL

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1171,7 +1171,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 23, 5, -0.4 }
+#define NOZZLE_TO_PROBE_OFFSET { 23, 5, -1.32 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
